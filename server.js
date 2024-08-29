65e2ae6296a3f2e13e70e5d906ed63f7882e423f
Keshaunj <keshaunjones48@gmail.com>
2024-08-28 21:04:34 -0400

    part 3 started

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 // server.js
 
-const express = require('express');
+const express = require("express");
 const app = express();
-
-app.get('/greetings/:christy' , (req, res) => {
-    const user = req.params.christy;
-    const greetingMessage = `Hello there, ${user}!`;
+// number 1
+app.get("/greetings/:christy", (req, res) => {
+  const user = req.params.christy;
+  const greetingMessage = `Hello there, ${user}!`;
   res.send(greetingMessage);
 });
+// roll route
+app.get("/roll/:number", (req, res) => {
+  const maxNumber = parseInt(req.params.number);
 
+  if (isNaN(maxNumber)) {
+    return res.status(400).send("You must specify a number.");
+  }
+  const rolledNumber = Math.floor(Math.random() * (maxNumber + 1));
+  res.send(`You rolled a ${rolledNumber}.`);
+});
+//number 3
+app.get("/collectibles/:index")
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
